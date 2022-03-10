f7f578c243a0d3350b47cd7a59f3ad5cfe24f8c8
Matheus Mendes <mattheus2015@yahoo.com.br>
2022-03-09 23:12:28 -0300

    feat: iniciando frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Inicio from "./pages/Inicio.vue";
+</script>
+
+<template>
+  <Inicio />
+</template>
+
+<style>
+</style>
---
+++ frontend/src/pages/Inicio.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Texto from "../components/Texto.vue"
+</script>
+
+<template>
+    <h1>Inicio do Pricefy</h1>
+    <Texto msg="Pricefy, ajudando você a pagar menos!" />
+</template>
+
+<style scoped>
+</style>
